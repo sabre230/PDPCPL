f1fb2a8e2983658e6256bf57119d24e0ac30ee5b
sabre230 <44850786+sabre230@users.noreply.github.com>
2024-01-14 17:35:28 -0800

    Delete run.bat

--- run.bat
+++
@@ -1,4 +0,0 @@
-:: All this does is make it easier to launch the update script and allow unsigned scripts for this one session
-:: You will need Powershell 5 or greater to run this
-pushd %~dp0
-start powershell -executionpolicy bypass -windowstyle hidden -noninteractive -nologo -file "update.ps1"
